5054c619c6732aed6f8c8b8e150e27287ecc2bb7
njourou <muirurishem@gmail.com>
2021-12-01 19:08:27 +0300

    update

---
+++ phine.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+mult=num1*num2
+print("The multiplication of the two numbers is: ",mult)
+c=num1+num2
+print("The sum of the two numbers is: ",c)
+d=num1-num2
+print("The difference of the two numbers is: ",d)
+e=num1/num2
+print("The division of the two numbers is: ",e)
+f=num1%num2
+print("The modulus of the two numbers is: ",f)
+g=num1**num2
+print("The power of the two numbers is: ",g)        
+h=num1//num2
+print("The floor division of the two numbers is: ",h)
